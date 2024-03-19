d49f2f9dbc535655e9168c9cf7fb196fee61cb24
Fiantso Harena <fiantsorav@gmail.com>
2024-03-19 20:52:14 +0300

    chore: add GET method

--- src/lib/pokemonAPI.ts
+++ src/lib/pokemonAPI.ts
@@ -1,13 +1,17 @@
 const POKEMON_API: string = 'https://pokeapi.co/api/v2/';
 
 export const getPokemonList = async () => {
-    const response: Response = await fetch(POKEMON_API + 'pokemon?limit=50&offset=0');
+    const response: Response = await fetch(POKEMON_API + 'pokemon?limit=50&offset=0', {
+        method: 'GET'
+    });
     const data: JSON = await response.json();
     return data;
 }
 
 export const getPokemon = async (name: string) => {
-    const response: Response = await fetch(POKEMON_API + 'pokemon/' + name);
+    const response: Response = await fetch(POKEMON_API + 'pokemon/' + name, {
+        method: 'GET'
+    });
     const data: JSON = await response.json();
     return data;
 }
