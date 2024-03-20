8708d5b442ed7260b4cce1293ec80d92edded234
Fiantso Harena <fiantsorav@gmail.com>
2024-03-20 08:18:00 +0300

    fix: map method in jsx field

--- src/components/PokemonGrid.tsx
+++ src/components/PokemonGrid.tsx
@@ -1,70 +1,57 @@
 'use client'
 
-import React, {useState, useEffect} from 'react';
-import Image from "next/image";
-import {Pokemon, Url} from '@/type/client';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+type Pokemon = {
+    name: string;
+    url: string;
+};
+
+type PokemonDetail = {
+    sprites: {
+        front_default: string;
+    };
+};
 
 const PokemonGrid = () => {
     const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-    const [url, setUrl] = useState<Url[]>([]);
+    const [pokemonDetails, setPokemonDetails] = useState<PokemonDetail[]>([]);
 
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon?limit=50&offset=0')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Failed to fetch pokemon list');
-                }
-                return response.json();
-            })
-            .then(data => {
-                setPokemonList(data.results);
-            })
-            .catch(error => {
-                console.error('Failed to retrieve pokemon list:', error);
-            });
+            .then(response => response.json())
+            .then(data => setPokemonList(data.results));
     }, []);
 
     useEffect(() => {
-        const fetchSprites = () => {
-            if (pokemonList.length > 0) {
-                const promises = pokemonList.map(async item => {
-                    const response = await fetch(item.url);
-                    return await response.json();
-                });
-                Promise.all(promises)
-                    .then(data => {
-                        console.log(data)
-                        setUrl(data);
-                    })
-                    .catch(error => {
-                        console.error('Failed to retrieve sprites:', error)
-                    });
-            }
+        if (pokemonList.length > 0) {
+            Promise.all(
+                pokemonList.map(pokemon =>
+                    fetch(pokemon.url).then(response => response.json())
+                )
+            ).then(data => {
+                console.log(pokemonDetails);
+                setPokemonDetails(data)
+            });
         }
-        fetchSprites();
     }, [pokemonList]);
 
     return (
         <div className="p-5 mt-5">
             <h1 className='text-3xl font-semibold text-center text-white mb-10'>See all Pokemon</h1>
             <div className="grid gap-5 grid-cols-5 text-center">
-                {pokemonList.map((pokemon) => (
-                    <>
-                        {url.map((sprite) => (
-                            <div key={pokemon.url} className='bg-white flex flex-col items-center text-center bg-opacity-15 rounded-lg shadow-2xl p-5'>
-                                <Image src={sprite.sprites.front_default} alt='sprite' width={150} height={150}/>
-                                <h1 className='text-white capitalize font-semibold mb-5'>
-                                    {pokemon.name}
-                                </h1>
-                            </div>
-                        ))
-                        }
-                    </>
+                {pokemonDetails.map((pokemon, index) => (
+                    <div key={pokemonList[index].url} className='bg-white flex flex-col items-center text-center bg-opacity-15 rounded-lg shadow-2xl p-5'>
+                        <Image src={pokemon.sprites.front_default} alt='sprite' width={150} height={150}/>
+                        <h1 className='text-white capitalize font-semibold mb-5'>
+                            {pokemonList[index].name}
+                        </h1>
+                    </div>
                 ))}
             </div>
         </div>
-    )
-        ;
+    );
 };
 
 export default PokemonGrid;
