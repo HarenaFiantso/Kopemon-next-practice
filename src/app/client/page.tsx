3c6db64ecc26d131080eae1836f74f86b89e6a7e
Fiantso Harena <fiantsorav@gmail.com>
2024-03-20 13:32:59 +0300

    chore: pokemon page for client component

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -2,8 +2,10 @@
 
 import React, {useEffect, useState} from "react";
 import Image from "next/image";
+import Link from "next/link";
+import {Pokeball} from "../../../public/assets";
 
-const ITEMS_PER_PAGE: number = 10;
+const ITEMS_PER_PAGE: number = 50;
 
 const fetchPokemonList = async (currentPage: number): Promise<Pokemon[]> => {
     const response: Response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${ITEMS_PER_PAGE}&offset=${(currentPage - 1) * ITEMS_PER_PAGE}`);
@@ -11,78 +13,49 @@ const fetchPokemonList = async (currentPage: number): Promise<Pokemon[]> => {
     return data.results;
 };
 
-const fetchPokemonDetails = async (pokemonList: Pokemon[]): Promise<PokemonDetail[]> => {
-    return await Promise.all(
-        pokemonList.map((pokemon: Pokemon) =>
-            fetch(pokemon.url).then(response => response.json())
-        )
-    );
-};
-
 const PokemonGrid: React.FC = () => {
     const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-    const [pokemonDetails, setPokemonDetails] = useState<PokemonDetail[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(1);
 
     useEffect(() => {
         fetchPokemonList(currentPage).then(setPokemonList);
     }, [currentPage]);
 
-    useEffect(() => {
-        if (pokemonList.length > 0) {
-            fetchPokemonDetails(pokemonList).then(setPokemonDetails);
-        }
-    }, [pokemonList]);
-
-    const lastPage: number = Math.ceil(pokemonList.length / ITEMS_PER_PAGE);
-
-    const changePage = (newPage: number) => {
-        setCurrentPage(newPage);
-    };
-
-    const nextPage = () => {
-        if (currentPage < lastPage) {
-            setCurrentPage((prevState: number) => prevState + 1);
-        }
-    };
-
-    const prevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage((prevState: number) => prevState - 1);
-        }
-    };
-
-
     return (
-        <div className="p-5 mt-5">
-            <h1 className='text-3xl font-semibold text-center text-white mb-10'>See all Pokemon</h1>
-            <div className="grid gap-5 grid-cols-5 text-center">
-                {pokemonDetails.map((pokemon: PokemonDetail, index: number) => (
-                    <div key={pokemonList[index].url}
-                         className='bg-white flex flex-col items-center text-center bg-opacity-15 rounded-lg shadow-2xl p-5'>
-                        <Image src={pokemon.sprites.front_default} alt='sprite' width={150} height={150}/>
-                        <h1 className='text-white capitalize font-semibold mb-5'>
-                            {pokemonList[index].name}
-                        </h1>
-                    </div>
-                ))}
-            </div>
-            {/* Pagination */}
-            <div className='w-full mt-10'>
-                <div className="text-white flex gap-5 justify-evenly items-center">
-                    <button onClick={prevPage} disabled={currentPage === 1}
-                            className='p-2 rounded-lg font-semibold bg-white text-black cursor-pointer'>Previous
-                    </button>
-                    {Array.from({length: lastPage}, (_, index) => (
-                        <button key={index} onClick={() => changePage(index + 1)}
-                                className={currentPage === index + 1 ? 'text-orange-500' : ''}>
-                            {index + 1}
-                        </button>
-                    ))}
-                    <button onClick={nextPage} disabled={currentPage === lastPage}
-                            className='p-2 rounded-lg font-semibold bg-white text-black cursor-pointer'>Next
-                    </button>
+        <div className='px-4 pt-12 pb-4 bg-gradient-to-r from-red-50 to-green-50 via-blue-50 min-h-screen'>
+            <div className="flex flex-col gap-5 items-center justify-center mb-10">
+                <div className="flex items-center gap-5">
+                    <Image src={Pokeball} alt="pokeball" width={70} height={50}/>
+                    <Link href={'/'} className="text-5xl font-semibold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text text-center ml-2">
+                        Kopémon
+                    </Link>
                 </div>
+                <p className='text-sm text-gray-500'>Welcome to Kopémon, see all Pokémon in one page</p>
+            </div>
+            <div className='flex justify-center items-center gap-10 flex-wrap w-full mt-8'>
+                {pokemonList.map((pokemon: Pokemon) => {
+                    const pokemonId: string = pokemon.url.split('/')[6];
+                    const paddedPokemonId: string = pokemonId.padStart(3, "0");
+
+                    return (
+                        <Link href={`/client/${pokemonId}`} key={pokemonId}>
+                            <div
+                                className="w-40 bg-white p-3 m-1 rounded-lg flex flex-col items-center text-center cursor-pointer shadow-sm hover:shadow-xl transition-all duration-300">
+                                <div className="w-20 h-20 md:w-24 md:h-24 flex justify-center items-center">
+                                    <img
+                                        src={`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/thumbnails-compressed/${paddedPokemonId}.png`}
+                                        alt="pokemon-thumbnail"
+                                        className="w-full h-full"
+                                        loading="lazy"
+                                    />
+                                </div>
+                                <h3 className="capitalize mt-2 text-orange-700 font-semibold text-center text-xs w-full">
+                                    {pokemon.name}
+                                </h3>
+                            </div>
+                        </Link>
+                    )
+                })}
             </div>
         </div>
     );
